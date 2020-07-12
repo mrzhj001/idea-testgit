267c0212d595452f054822f32fbef74f32ce090f
root <1316626683@qq.com>
2020-07-12 19:02:05 +0800

    这是将我们的Java工程第一次提交到了本地仓库

---
+++ idea-git-test/src/com/yd/user/User.java
@@ -0,0 +1,48 @@
+package com.yd.user;
+
+/**
+ * @author jay
+ * @date 2020/7/12
+ **/
+public class User {
+    private int age ;
+    private  String  name;
+    private  String  sex;
+
+    public User() {
+    }
+
+    public User(int age, String name, String sex) {
+        this.age = age;
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("idea 测试远程仓库的提交功能");
+    }
+}
